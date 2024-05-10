11170ca8892fe8a6ef4e2b51cbf0b966e85ce888
Pedro Scarpin Ribeiro <pedroscarpinribeiro@gmail.com>
2024-05-10 15:35:23 -0300

    add athlete routes

--- workout_api/workout_api/atleta/schemas.py
+++ workout_api/workout_api/atleta/schemas.py
@@ -1,6 +1,8 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
 from pydantic import Field, PositiveFloat
+from workout_api.categorias.schemas import CategoriaIn
+from workout_api.centro_treinamento.schemas import CentroTreinamentoAtleta
 from workout_api.contrib.schemas import BaseSchema, OutMixin
 
 
@@ -17,6 +19,10 @@ class Atleta(BaseSchema):
         PositiveFloat, Field(description="Altura do atleta", example="1.81")
     ]
     sexo: Annotated[str, Field(description="Sexo do atleta", example="M", max_length=1)]
+    categoria: Annotated[CategoriaIn, Field(description="Categoria do atleta")]
+    centro_treinamento: Annotated[
+        CentroTreinamentoAtleta, Field(description="CT do atleta")
+    ]
 
 
 class AtletaIn(Atleta):
@@ -25,3 +31,13 @@ class AtletaIn(Atleta):
 
 class AtletaOut(Atleta, OutMixin):
     pass
+
+
+class AtletaUpdate(BaseSchema):
+    nome: Annotated[
+        Optional[str],
+        Field(None, description="Nome do atleta", example="Joao", max_length=50),
+    ]
+    idade: Annotated[
+        Optional[int], Field(None, description="Idade do atleta", example="21")
+    ]
