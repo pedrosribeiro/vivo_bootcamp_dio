dc1baa3d1d24c72a509a3bcd1cf17eb303c4ee32
Pedro Scarpin Ribeiro <pedroscarpinribeiro@gmail.com>
2024-05-18 16:09:27 -0300

    feat: add product endpoint and integration tests

--- tdd_project/tests/factories.py
+++ tdd_project/tests/factories.py
@@ -1,2 +1,21 @@
 def product_data():
-    return {"name": "iPhone 14 Pro Max", "quantity": 10, "price": 8500, "status": True}
+    return {
+        "name": "iPhone 14 Pro Max",
+        "quantity": 10,
+        "price": 8500,
+        "status": True,
+    }
+
+
+def products_data():
+    return [
+        {"name": "iPhone 11 Pro Max", "quantity": 20, "price": 4500, "status": True},
+        {"name": "iPhone 12 Pro Max", "quantity": 15, "price": 5500, "status": True},
+        {"name": "iPhone 13 Pro Max", "quantity": 5, "price": 6500, "status": True},
+        {
+            "name": "iPhone 15 Pro Max",
+            "quantity": 3,
+            "price": 10.500,
+            "status": False,
+        },
+    ]
